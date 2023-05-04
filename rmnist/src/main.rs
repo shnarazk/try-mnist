a14f9cf29842ea286fc291fc62aeb26436633ca0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-04 20:32:03 +0900

    modified:   rmnist/src/main.rs

--- rmnist/src/main.rs
+++ rmnist/src/main.rs
@@ -1,8 +1,10 @@
 #![allow(unused_imports)]
 use {
     dfdx::{data::*, optim::Adam, prelude::*, tensor::AutoDevice},
+    indicatif::ProgressIterator,
     mnist::*,
     rand::prelude::{SeedableRng, StdRng},
+    std::time::Instant,
 };
 
 #[derive(Debug)]
@@ -38,6 +40,8 @@ type Mlp = (
     Linear<32, 10>,
 );
 
+const BATCH_SIZE: usize = 32;
+
 fn main() {
     println!("Hello, world!");
     dfdx::flush_denormals_to_zero();
@@ -46,19 +50,48 @@ fn main() {
         .unwrap_or_else(|| "../datasets/MNIST/raw".to_string());
     println!("Loading mnist from args[1] = {mnist_path}");
     let dev = AutoDevice::default();
-    let mut _rng = StdRng::seed_from_u64(0);
-    let mut _model = dev.build_module::<Mlp, f32>();
-    // let mut opt = Adam::new(&model, Default::default());
+    let mut rng = StdRng::seed_from_u64(0);
+    let mut model = dev.build_module::<Mlp, f32>();
+    let mut grads = model.alloc_grads();
+    let mut opt = Adam::new(&model, Default::default());
 
     let dataset = MnistTrainSet::new(&mnist_path);
     println!("Found {:?} training images", dataset.len());
 
-    let _preprocess = |(img, lbl): <MnistTrainSet as ExactSizeDataset>::Item<'_>| {
+    let preprocess = |(img, lbl): <MnistTrainSet as ExactSizeDataset>::Item<'_>| {
         let mut one_hotted = [0.0; 10];
         one_hotted[lbl] = 1.0;
         (
-            dev.tensor_from_vec(img, (Const::<787>,)),
+            dev.tensor_from_vec(img, (Const::<784>,)),
             dev.tensor(one_hotted),
         )
     };
+    for i_epoch in 0..10 {
+        let mut total_epoch_loss = 0.0;
+        let mut num_batches = 0;
+        let start = Instant::now();
+        for (img, lbl) in dataset
+            .shuffled(&mut rng)
+            .map(preprocess)
+            .batch_exact(Const::<BATCH_SIZE>)
+            .collate()
+            .stack()
+            .progress()
+        {
+            let logits = model.forward_mut(img.traced(grads));
+            let loss = cross_entropy_with_logits_loss(logits, lbl);
+            total_epoch_loss += loss.array();
+            num_batches += 1;
+            grads = loss.backward();
+            opt.update(&mut model, &grads).unwrap();
+            model.zero_grads(&mut grads);
+        }
+        let dur = Instant::now() - start;
+        println!(
+            "Epoch {i_epoch} in {:?} ({:.3} batches/s): avg sample loss {:.5}",
+            dur,
+            num_batches as f32 / dur.as_secs_f32(),
+            BATCH_SIZE as f32 * total_epoch_loss / num_batches as f32,
+        );
+    }
 }
