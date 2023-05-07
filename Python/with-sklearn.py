284f4f829869a73f1a188a0d50ace0013b4a86c9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-07 09:07:09 +0900

    new file:   Python/with-sklearn.py

---
+++ Python/with-sklearn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sklearn.datasets
+import sklearn.svm
+import PIL.Image
+import numpy
+
+def imageToData(filename):
+    grayImage = PIL.Image.open(filename).convert("L")
+    grayImage = grayImage.resize((8, 8), PIL.Image.Resampling.LANCZOS)
+    numImage = numpy.asarray(grayImage, dtype = float)
+    numImage = 16 - numpy.floor(17 * numImage / 256)
+    numImage = numImage.flatten()
+    return numImage
+
+def predictDigits(data):
+    digits = sklearn.datasets.load_digits()
+    clf = sklearn.svm.SVC(gamma = 0.001)
+    clf.fit(digits.data, digits.target)
+    n = clf.predict([data])
+    print("result: ", n)
+
+predictDigits(imageToData("2.png"))
